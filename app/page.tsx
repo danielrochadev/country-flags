6c5733e30b4669be3086504e900d10c347dc6c69
Daniel Rocha <danielldante@gmail.com>
2025-01-22 00:45:48 -0300

    add Footer and Header components

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
+import { Header, Footer } from "./components";
 
 export default function Home() {
   return (
     <>
-      <header>This is the header</header>
+      <Header/>
       <main className="flex-1">
         This is the main
       </main>
-      <footer>This is the footer</footer>
-    
+      <Footer/>
     </>
   );
 }
