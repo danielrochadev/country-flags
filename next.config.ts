b98e80644735cf760ac5b47d814ea21623556121
Daniel Rocha <danielldante@gmail.com>
2025-01-28 15:30:17 -0300

    Change next.config for classnames no obfuscate

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    const rules = config.module.rules
+      .find((rule) => typeof rule.oneOf === 'object')
+      .oneOf.filter((rule) => Array.isArray(rule.use));
+
+    rules.forEach((rule) => {
+      rule.use.forEach((moduleLoader) => {
+        if (
+          moduleLoader.loader?.includes('css-loader') && 
+          !moduleLoader.loader?.includes('postcss-loader')
+        ) {
+          moduleLoader.options.modules = {
+            ...moduleLoader.options.modules,
+            localIdentName: '[name]__[local]'
+          };
+        }
+      });
+    });
+
+    return config;
+  }
 };
 
 export default nextConfig;
